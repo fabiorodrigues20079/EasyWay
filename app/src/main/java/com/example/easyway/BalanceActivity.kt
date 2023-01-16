13725af4a5d991b1179948a47aac534761050623
Fábio <fabioalexandre217@gmail.com>
2023-01-16 03:39:42 +0000

    NovosXML's

---
+++ app/src/main/java/com/example/easyway/BalanceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.easyway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BalanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_balance)
+    }
+}
